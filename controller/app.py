654cbafc9f340833a6eb8bcd6a8cd43a6326ecc2
Domi <62677969+R3puLsiv3@users.noreply.github.com>
2023-10-11 10:25:32 +0200

    Created basic MVC file structure. App window main loop implemented.

---
+++ controller/app.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Turing Machine")
---
+++ controller/main.py
@@ -0,0 +1,11 @@
+# https://www.pythontutorial.net/tkinter/tkinter-mvc/
+import app
+
+
+def main():
+    application = app.App()
+    application.mainloop()
+
+
+if __name__ == '__main__':
+    main()
