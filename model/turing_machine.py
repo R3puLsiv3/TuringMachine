ea7af3a5a20de3e8b61e1e3d5b54ec1593f6cf53
Domi <62677969+R3puLsiv3@users.noreply.github.com>
2023-10-15 17:08:11 +0200

    Extended turing machine to allow for turing machines with multiple tapes.

--- model/turing_machine.py
+++ model/turing_machine.py
@@ -1,3 +1,4 @@
+import model
 
 
 class TuringMachine(object):
@@ -6,17 +7,19 @@ class TuringMachine(object):
         if not hasattr(cls, "instance"):
             cls.instance = super(TuringMachine, cls).__new__(cls)
 
-            # For the current state s, states[s] is a collection of possible transitions of the form
+            # For the current state s and tape t states[s][t] is a collection of possible transitions of the form:
             # current input -> [output, head movement, next state]
-            cls.instance.states: list[dict[str, list[str, str, int]]] = []
+            cls.instance.states: list[list[dict[str, list[str, str, int]]]] = []
+            cls.instance.halting_states: set = {}
             cls.instance.current_state = 0
-            cls.instance.blank_symbol = "_"
+            cls.instance.blank_symbol = " "
         return cls.instance
 
     def get_tape_alphabet(self) -> set[str]:
-        return set([self.states[i][key][0] for i in range(len(self.states)) for key in self.states[i]])\
+        return set([self.states[state][tape][inp][0] for state in range(len(self.states))
+                    for tape in range(len(self.states[state])) for inp in self.states[state][tape]])\
             .union(self.get_input_alphabet()).union(self.blank_symbol)
 
     def get_input_alphabet(self) -> set[str]:
-        return set([key for i in range(len(self.states)) for key in self.states[i]])\
-            .difference(self.blank_symbol)
+        return set([key for state in range(len(self.states)) for tape in range(len(self.states[state]))
+                   for key in self.states[state][tape]]).difference(self.blank_symbol)
--- tests/test_turing_machine.py
+++ tests/test_turing_machine.py
@@ -6,12 +6,13 @@ class MyTestCase(unittest.TestCase):
 
     def setUp(self):
         self.tm = model.TuringMachine()
-        self.tm.states.append({"1": ["2", "L", 2], "2": ["b", "N", 3], "_": ["2", "R", 1]})
-        self.tm.states.append({"1": ["1", "R", 3], "2": ["_", "R", 3], "_": ["_", "L", 2]})
-        self.tm.states.append({"1": ["c", "L", 1], "2": ["2", "N", 1], "_": ["a", "N", 3]})
+        self.tm.states.append([{"1": ["2", "L", 2], "2": ["b", "N", 3], " ": ["2", "R", 1]},
+                               {"1": ["2", "R", 2], "2": ["b", "L", 3], " ": ["2", "N", 1]}])
+        self.tm.states.append([{"1": ["1", "R", 3], "2": [" ", "R", 3], " ": [" ", "L", 2]}])
+        self.tm.states.append([{"1": ["c", "L", 1], "2": ["2", "N", 1], " ": ["a", "N", 3]}])
 
     def test_get_tape_alphabet(self):
-        self.assertSetEqual(self.tm.get_tape_alphabet(), {"_", "1", "2", "a", "b", "c"}, "incorrect tape alphabet")
+        self.assertSetEqual(self.tm.get_tape_alphabet(), {" ", "1", "2", "a", "b", "c"}, "incorrect tape alphabet")
 
     def test_get_input_alphabet(self):
         self.assertSetEqual(self.tm.get_input_alphabet(), {"1", "2"}, "incorrect input alphabet")
